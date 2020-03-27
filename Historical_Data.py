52f102a83c51960b58f52f2a3e0707187902db7f
sschott <sschott20@students.hopkins.edu>
2020-03-27 15:48:25 -0400

    improvements

--- Historical_Data.py
+++ Historical_Data.py
@@ -8,7 +8,8 @@ import shutil
 # period1 and period2 are Unix time stamps for your start and end date
 # interval is the data retrieval interval (this can be either 1d, 1w or 1m)
 
-def pull_all_data(): 
+
+def pull_all_data():
     with open("old_data/Lists/sp5002012.txt", "r") as file:
         for line in file:
             company = line.strip()
@@ -16,20 +17,23 @@ def pull_all_data():
             print("\n")
             pull_historical_data(company)
             try:
-                filename = make_filename((company), "data")
+                filename = make_filename((company))
                 src = "C:/Users/sebal/Desktop/Coding/Python/ML-Trading"
-                dst = "C:/Users/sebal/Desktop/Coding/Python/ML-Trading/data/S&P"
+                dst = "C:/Users/sebal/Desktop/Coding/Python/ML-Trading/data"
                 shutil.move(os.path.join(src, filename), os.path.join(dst, filename))
-                # shutil.move(make_filename(company), "current_data")
             except:
                 print("Error moving file")
-            
+
+
 def pull_specific_stocks():
-    stocks = ["ZM", "TSLA", "PG"]
+    stocks = ["ZM", "TSLA", "PG", "SPY"]
     for stock in stocks:
         pull_historical_data(stock)
         try:
-            shutil.move(make_filename(stock), "current_data")
+            filename = make_filename((stock))
+            src = "C:/Users/sebal/Desktop/Coding/Python/ML-Trading"
+            dst = "C:/Users/sebal/Desktop/Coding/Python/ML-Trading/data"
+            shutil.move(os.path.join(src, filename), os.path.join(dst, filename))
         except Exception as e:
             print("Error moving file")
             print(e)
@@ -37,9 +41,10 @@ def pull_specific_stocks():
 
 current_time = str(math.floor(time.time()))
 
-# Times: 
+# Times:
 # 2019-01-01 : 1546300800
 # 2000-01-01 : 946684800
+
 
 def make_url(
     ticker_symbol, period1="1546300800", period2=current_time, interval="1d",
@@ -50,14 +55,14 @@ def make_url(
     )
 
 
-def make_filename(ticker_symbol, directory="S&P"):
+def make_filename(ticker_symbol):
     return ticker_symbol + ".csv"
 
 
-def pull_historical_data(ticker_symbol, directory="S&P"):
+def pull_historical_data(ticker_symbol):
     try:
         urllib.request.urlretrieve(
-            make_url(ticker_symbol), make_filename(ticker_symbol, directory)
+            make_url(ticker_symbol), make_filename(ticker_symbol)
         )
     # except urllib.request.ContentTooShortError as e:
     #     outfile = open(make_filename(ticker_symbol, directory), "w")
@@ -68,5 +73,5 @@ def pull_historical_data(ticker_symbol, directory="S&P"):
 
 
 if __name__ == "__main__":
-    pull_all_data()
-    # pull_specific_stocks()
+    # pull_all_data()
+    pull_specific_stocks()
--- Normalized_Plots.py
+++ Normalized_Plots.py
@@ -1,59 +1,86 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import requests
 
-def symbol_to_path(symbol, base_dir="current_data"):
-    """Return CSV file path given ticker symbol."""
+
+def symbol_to_path(symbol, base_dir="data"):
+    """Return CSV file path given ticker symbol.
+    If CSV is not already downloaded, will download from yahoo finance"""
+
     return os.path.join(base_dir, "{}.csv".format(str(symbol)))
+
 
 def plot_selected(df, columns, start_index, end_index):
     """Plot the desired columns over index values in the given range."""
-    df = df.loc[start_index : end_index, columns]
+    df = df.loc[start_index:end_index, columns]
     plot_data(df)
+
 
 def get_data(symbols, dates):
     """Read stock data (adjusted close) for given symbols from CSV files."""
     df = pd.DataFrame(index=dates)
-    if 'SPY' not in symbols:  # add SPY for reference, if absent
-        symbols.insert(0, 'SPY')
-    
+    if "SPY" not in symbols:  # add SPY for reference, if absent
+        symbols.insert(0, "SPY")
+
     for symbol in symbols:
-        df_temp = pd.read_csv(symbol_to_path(symbol), 
-        index_col="Date", 
-        parse_dates = True,
-        usecols =["Date", "Adj Close"],
-        na_values = ["nan"]
+        df_temp = pd.read_csv(
+            symbol_to_path(symbol),
+            index_col="Date",
+            parse_dates=True,
+            usecols=["Date", "Adj Close"],
+            na_values=["nan"],
         )
-       
-        df_temp = df_temp.rename(columns={"Adj Close" : symbol})
+        company_name = get_symbol(symbol)
+
+        df_temp = df_temp.rename(columns={"Adj Close": company_name})
+
         df = df.join(df_temp)
-        
-        if symbol == "SPY":
-            df = df.dropna(subset=['SPY'])
+
+        if company_name == "SPDR S&P 500 ETF Trust":
+            df = df.dropna(subset=["SPDR S&P 500 ETF Trust"])
+
     return df
 
 
 def test_run():
     # Define a date range
-    dates = pd.date_range('2019-05-01', '2020-03-26')
+    dates = pd.date_range("2019-11-01", "2020-03-26")
 
     # Choose stock symbols to read
-    symbols = ['SPY', 'TSLA', 'ZM', 'PG']
-    
+    symbols = ["SPY", "TSLA", "ZM", "KMB", "COST", "UAL"]
+
     # Get stock data
     df = get_data(symbols, dates)
-    # plot_data(df)
-    plot_data(normalize_data(df))
+
+    plot_data(df)
+
+    # plot_data(normalize_data(df))
+
 
 def normalize_data(df):
     df = df / df.iloc[0]
     return df
 
-def plot_data(df, title = 'Stock Prices'):
+
+def plot_data(df, title="Stock Prices"):
     ax = df.plot(title=title, fontsize=10)
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Price')
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Adjusted Close Price (USD)")
     plt.show()
+
+
+def get_symbol(symbol):
+    url = "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={}&region=1&lang=en".format(
+        symbol
+    )
+
+    result = requests.get(url).json()
+
+    for x in result["ResultSet"]["Result"]:
+        if x["symbol"] == symbol:
+            return x["name"]
+
 
 if __name__ == "__main__":
     test_run()
