79adf6b6573f6e0198588534521eb06d99f357e7
Kadin Zhang <cykadin@gmail.com>
2020-03-28 21:38:40 -0400

    add project 2

--- Portfolio Statistics/analysis.py
+++ Portfolio Statistics/analysis.py
@@ -11,19 +11,32 @@ def portfolio_statistics(
     risk_free_rate,
     sample_frequency,
 ):
+    # Get and fill data
     dates = pd.date_range(start_date, end_date)
     prices = get_old_data(symbols, dates)
-
     prices.fillna(method="ffill", inplace=True)
     prices.fillna(method="bfill", inplace=True)
-
     prices = prices[symbols]
-    # normed = prices / prices[0]
-    print(prices.mean())
-    return
+
+    # Calculate portfolio values
+    normed = prices / prices.values[0]
+    alloced = normed * allocations
+    pos_values = alloced * start_value
+    port_vals = pos_values.sum(axis = 1)
+
+    # Calculate daily returns
+    daily_rets = (port_vals[1:] / port_vals.values[:-1]) - 1
+
+    # Calculate portfolio statistics
+    cumulative_return = (port_vals[-1] / port_vals[0]) - 1
+    average_daily_return = daily_rets.mean()
+    risk = daily_rets.std()
+    sharpe_ratio = (daily_rets - risk_free_rate).mean() / risk
+
+    return cumulative_return, average_daily_return, risk, sharpe_ratio
 
 
-portfolio_statistics(
+cumulative_return, average_daily_return, risk, sharpe_ratio = portfolio_statistics(
     "2010-01-22",
     "2010-02-22",
     ["GOOG", "AAPL", "GLD", "XOM"],
@@ -32,3 +45,8 @@ portfolio_statistics(
     0,
     252,
 )
+
+print("Cumulative return: ", cumulative_return)
+print("Average daily return: ", average_daily_return)
+print("Risk: ", risk)
+print("Sharpe ratio: ", sharpe_ratio)
